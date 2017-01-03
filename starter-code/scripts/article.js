d5ef2648eb98dd4731a060471b57329ef4e7c6c7
radenska <yana_radenska@yahoo.com>
2017-01-03 15:33:21 -0800

    stupid a href selectors! got the last two TODOs working

--- starter-code/scripts/article.js
+++ starter-code/scripts/article.js
@@ -1,24 +1,28 @@
 var articles = [];
 
 function Article (options) {
-/* TODO: This is our Model constructor. It will take in
+/* DONE: This is our Model constructor. It will take in
    our source data from blogArticles and instantiate a
    new Object according to this new definition. options is
    a placeholder for the object that will ultimately be
    passed in. Use all of the properties in blogArticles
-   to populate the new Article data that we'll use.  */      
-   this.category = options.category
-   this.author = options.author
-   this.authorUrl = options.authorUrl
-   this.publishedOn = options.publishedOn;
-   this.body = options.body;
-   this.title = options.title;
-};
+   to populate the new Article data that we'll use.  */
+  this.category = options.category;
+  this.author = options.author;
+  this.authorUrl = options.authorUrl;
+  this.publishedOn = options.publishedOn;
+  this.body = options.body;
+  this.title = options.title;
+}
 
 Article.prototype.toHtml = function() {
   var $newArticle = $('article.template').clone();
   $newArticle.attr('data-category', this.category);
-  /* TODO: We also need to fill in:
+  $newArticle.find('h1').text(this.title);
+  $newArticle.find('address a').text(this.author);
+  $newArticle.find('address a').attr('href', this.authorUrl);
+  $newArticle.find('.article-body').html(this.body);
+  /* DONE: We also need to fill in:
   1. author name
   2. author url
   3. article title
@@ -26,8 +30,9 @@ Article.prototype.toHtml = function() {
   5. publication*/
   $newArticle.find('time[pubdate]').attr('title', this.publishedOn);
   $newArticle.find('time').text('about ' + parseInt((new Date() - new Date(this.publishedOn))/60/60/24/1000) + ' days ago');
-/* TODO: This cloned article is no longer a template, as it now
+/* DONE: This cloned article is no longer a template, as it now
 has real data attached to it. Remove the class from this new article! */
+  $newArticle.removeClass('template');
   return $newArticle;
 };
 
